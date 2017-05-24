2a49715947a341a56a6964b5786c20cee903d323
Harris <harris.shallcross@gmail.com>
2017-05-24 20:22:31 +0100

    Added C# program

---
+++ CameraViewT1/CameraViewT1/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: harri
+ * Date: 20/05/2017
+ * Time: 02:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace CameraViewT1
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label labelframe;
+		private System.Windows.Forms.Label labelfps;
+		private System.Windows.Forms.Label label3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.labelframe = new System.Windows.Forms.Label();
+			this.labelfps = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1Paint);
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Location = new System.Drawing.Point(13, 13);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(661, 520);
+			this.tabControl1.TabIndex = 1;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.pictureBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(653, 494);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "Image";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.richTextBox1);
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(653, 494);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "Pixels";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(7, 7);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(640, 481);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(680, 35);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(54, 23);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Frame: ";
+			// 
+			// labelframe
+			// 
+			this.labelframe.Location = new System.Drawing.Point(740, 35);
+			this.labelframe.Name = "labelframe";
+			this.labelframe.Size = new System.Drawing.Size(54, 23);
+			this.labelframe.TabIndex = 4;
+			this.labelframe.Text = "0";
+			// 
+			// labelfps
+			// 
+			this.labelfps.Location = new System.Drawing.Point(740, 58);
+			this.labelfps.Name = "labelfps";
+			this.labelfps.Size = new System.Drawing.Size(54, 23);
+			this.labelfps.TabIndex = 6;
+			this.labelfps.Text = "0";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(680, 58);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(54, 23);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "FPS:";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(831, 546);
+			this.Controls.Add(this.labelfps);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.labelframe);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.tabControl1);
+			this.Name = "MainForm";
+			this.Text = "CameraViewT1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFormFormClosing);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ CameraViewT1/CameraViewT1/MainForm.cs
@@ -0,0 +1,233 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: harri
+ * Date: 20/05/2017
+ * Time: 02:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CameraViewT1
+{
+	public partial class MainForm : Form
+	{
+		SerialPort sp = null;
+		Thread spthread = null;
+		uint[] frame;
+		public MainForm()
+		{
+			InitializeComponent();
+			frame = new uint[640*480];
+			pictureBox1.Image = new Bitmap(640, 480, PixelFormat.Format16bppRgb565);
+			
+			if(SerialPort.GetPortNames().Length>0){
+				//sp = new SerialPort(SerialPort.GetPortNames()[0], 1228800);
+				sp = new SerialPort(SerialPort.GetPortNames()[0], 6000000);
+				sp.ReadTimeout = -1;
+				sp.ReceivedBytesThreshold = sp.ReadBufferSize = 640*480*2+10;
+				sp.Open();
+				sp.DiscardInBuffer();
+				spthread = new Thread(new ThreadStart(SpThread));
+				spthread.Start();
+				
+				Debug.WriteLine(sp.PortName);
+			}
+			else{
+				MessageBox.Show("No COM ports.");
+			}
+		}
+		
+		uint[] IDCT(Int16[] array){
+			double[,] coeff = {
+				{0.354, 0.354, 0.354, 0.354, 0.354, 0.354, 0.354, 0.354},
+				{0.490, 0.416, 0.278, 0.098, -0.098, -0.278, -0.416, -0.490},
+				{0.462, 0.191, -0.191, -0.462, -0.462, -0.191, 0.191, 0.462},
+				{0.416, -0.098, -0.490, -0.278, 0.278, 0.490, 0.098, -0.416},
+				{0.354, -0.354, -0.354, 0.354, 0.354, -0.354, -0.354, 0.354},
+				{0.278, -0.490, 0.098, 0.416, -0.416, -0.098, 0.490, -0.278},
+				{0.191, -0.462, 0.462, -0.191, -0.191, 0.462, -0.462, 0.191},
+				{0.098, -0.278, 0.416, -0.490, 0.490, -0.416, 0.278, -0.098}
+			};
+			
+			uint[] outv = new uint[8];
+			double sum = 0;
+			
+			for(int n = 0; n<8; n++){
+				sum = 0;
+				for(int m = 0; m<8; m++){
+					sum += ((double)array[m])*(coeff[n,m]);
+				}
+				
+				sum = sum*255/31;
+				
+				if(sum<0) outv[n] = 0;
+				else if(sum<255) outv[n] = (uint) sum;
+				else outv[n] = 255;
+			}
+			
+			return outv;
+		}
+		
+		int XPIX = 640;
+		int YPIX = 480;
+		
+		int fc = 0;
+		long ms1;
+		double fps;
+		void SpThread(){
+			byte[] btr = new byte[640*480*2];
+			ms1 = 0;
+			while(true){
+				Debug.WriteLine("New frame " + fc);
+				int bytes = 0;
+				while(sp.BytesToRead<XPIX*YPIX*2){
+					bytes = sp.BytesToRead;
+					Thread.Sleep(10);
+					Debug.WriteLine(sp.BytesToRead);
+					
+					if(bytes == sp.BytesToRead) sp.Write(new byte[1]{0xaa}, 0, 1);
+					while(sp.BytesToWrite>0) Thread.Sleep(2);
+				}
+				
+				sp.Read(btr, 0, XPIX*YPIX*2);
+				
+				for(int n = 0; n<XPIX*YPIX*2; n+=2){
+					//uint v = (uint)BitConverter.ToUInt16(new byte[2]{btr[n], btr[n+1]}, 0);
+					uint v = (uint)BitConverter.ToUInt16(new byte[2]{btr[n+1], btr[n]}, 0);
+					frame[n/2] = v;
+				}
+				/*
+				sp.Write(new byte[1]{0xaa}, 0, 1);
+				while(sp.BytesToWrite>0) Thread.Sleep(2);
+				
+				int bytes = 0;
+				while(sp.BytesToRead<640*480){
+					bytes = sp.BytesToRead;
+					Thread.Sleep(10);
+					Debug.WriteLine(sp.BytesToRead);
+					
+					if(bytes == sp.BytesToRead) sp.Write(new byte[1]{0xaa}, 0, 1);
+					while(sp.BytesToWrite>0) Thread.Sleep(2);
+				}
+				
+				sp.Read(btr, 0, 640*480);
+				
+				for(int n = 0; n<640*480; n++){
+					frame[n] = btr[n];
+				}*/
+				
+				
+				/*while(sp.BytesToRead<480*(640*2*3/8)){
+					Thread.Sleep(100);
+					Debug.WriteLine(sp.BytesToRead);
+				}
+				
+				sp.Read(btr, 0, 480*(640*2*3/8));
+				
+				uint fpos = 0;
+				uint[] rp, gp, bp;
+				Int16[] pb = new Int16[8];
+				
+				pb[2]=pb[3]=pb[4]=pb[5]=pb[6]=pb[7] = 0;
+				for(int n = 0; n<480*(640*2*3/8); n+=6){
+					pb[0] = Convert.ToInt16(btr[n]);
+					pb[1] = Convert.ToInt16(btr[n+1]);
+					rp = IDCT(pb);
+					
+					pb[0] = Convert.ToInt16(btr[n+2]);
+					pb[1] = Convert.ToInt16(btr[n+3]);
+					gp = IDCT(pb);
+					
+					pb[0] = Convert.ToInt16(btr[n+4]);
+					pb[1] = Convert.ToInt16(btr[n+5]);
+					bp = IDCT(pb);
+					
+					for(int m = 0; m<8; m++){
+						uint pix = (bp[m]) | (gp[m]<<8) | (rp[m]<<16);
+						frame[fpos++] = pix;
+					}
+				}*/
+				
+				if((int)Invoke(new Func<int>(() => tabControl1.SelectedIndex)) == 1){
+					richTextBox1.Invoke(new Action(() => richTextBox1.Text = string.Join(" ", frame)));
+				}
+				else{
+					pictureBox1.Invoke(new Action(pictureBox1.Invalidate));
+				}
+				fps = 1000/(double)((long)(DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds - ms1);
+				ms1 = (long)(DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds;
+				labelframe.Invoke(new Action(() => labelframe.Text = fc.ToString()));
+				labelfps.Invoke(new Action(() => labelfps.Text = fps.ToString()));
+				
+				fc++;
+				sp.DiscardInBuffer();
+				Thread.Sleep(10);
+			}
+		}
+		
+		
+		void PictureBox1Paint(object sender, PaintEventArgs e)
+		{
+			uint r,g,b;
+			uint pix;
+			
+			/*for(int y = 0; y<480; y++){
+				for(int x = 0; x<640; x++){
+					pix = frame[x+y*640];
+					((Bitmap)pictureBox1.Image).SetPixel(x,y,Color.FromArgb(255,(int)(pix>>16)&255,(int)(pix>>16)&255,(int)pix&255));
+				}
+			}*/
+			
+			for(int y = 0; y<480; y++){
+				for(int x = 0; x<640; x++){
+					pix = frame[x*XPIX/640+y*XPIX];
+					//pix = frame[(x>>2) + (y>>2)*XPIX];
+					//pix = frame[x + y*XPIX];
+					r = (pix>>11)*255/31;
+					g = ((pix>>5)&63)*255/63;
+					b = (pix&31)*255/31;
+					//r = (pix>>5)*255/7;
+					//g = ((pix>>2)&7)*255/7;
+					//b = (pix&3)*255/3;
+					((Bitmap)pictureBox1.Image).SetPixel(x,y,Color.FromArgb(255,(int)r,(int)g,(int)b));
+				}
+			}
+			
+			/*for(int y = 0; y<480; y++){
+				for(int x = 0; x<640; x++){
+					//pix = frame[(x>>1)+(y>>1)*320];
+					pix = frame[x+y*640];
+					r = (pix>>5)*255/7;
+					g = ((pix>>2)&7)*255/7;
+					b = (pix&3)*255/3;
+					//r = pix & 0xE0;
+					//g = ((pix>>2)&7)<<5;
+					//b = (pix&3)<<6;
+					((Bitmap)pictureBox1.Image).SetPixel(x,y,Color.FromArgb(255,(int)r,(int)g,(int)b));
+				}
+			}*/
+		}
+		
+		void MainFormFormClosing(object sender, FormClosingEventArgs e)
+		{
+			if(spthread != null && spthread.IsAlive){
+				spthread.Abort();
+				spthread.Join();
+			}
+			
+			if(sp != null && sp.IsOpen){
+				sp.Close();
+			}
+		}
+	}
+}
+
+
